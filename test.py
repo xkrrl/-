6ee3954f4d77ca4b4e9ca972902cfff34181c15b
xkrrl <85406998+xkrrl@users.noreply.github.com>
2021-06-07 01:25:16 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pyupbit
-
-access = "qsBHWvmgqXyvMHMheKov2egxwvoWT00TEa5kYW09"          # 본인 값으로 변경
-secret = "Z47FcEu3kxvIhFEfKBvxiwQFCsvx6bWFa6XmThJu"          # 본인 값으로 변경
-upbit = pyupbit.Upbit(access, secret)
-
-print(upbit.get_balance("KRW-XRP"))     # KRW-XRP 조회
-print(upbit.get_balance("KRW"))         # 보유 현금 조회
